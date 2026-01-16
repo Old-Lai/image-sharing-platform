0f3d3b75057d79ffcc7f1d79cb770520de973643
oldlai <henrylai8115@gmail.com>
2026-01-16 08:04:22 +0000

    add cors

--- backend/app.ts
+++ backend/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from "cors";
 import path from 'path';
 
 import uploadRoutes from './routes/upload.route';
@@ -6,6 +7,7 @@ import imageRoutes from './routes/image.route';
 import errorMiddleware from './middlewares/error.middleware';
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
